279b8064f2f12e961b517d568449d0ece7d13313
Blancaslan <68610191+Blancaslan@users.noreply.github.com>
2023-01-07 22:04:19 +0000

    It works!

--- src/road.js
+++ src/road.js
@@ -1,4 +1,4 @@
-// Class for initializing the map and modifying
+// Class for the map; to show data, to give data to and to alter data
 class Map {
     constructor(map) {
         this.map = map
@@ -9,10 +9,12 @@ class Map {
     }
 
     showMapIndex(y, x) {
-        if (this.map[y][x] === undefined) {
-            return false
+        try {
+            return this.map[y][x]
         }
-        return this.map[y][x]
+        catch {
+            return "Error position doesn't exist, try somewhere else idiot!"
+        }
     }
 
     deleteIndex(positionX, positionY) {
@@ -33,27 +35,25 @@ class Road {
         this.isRoadAccessable = isRoadAccessable
     }
 
-    // checks if a road that is nearby is accessable and if yes then makes this road accessable
+    // checks if a road nearby is accessable then makes this road accessable
     roadCheck(map) {
         this.isRoadAccessable = (
             // checks up
-            roadAccessableCheck(this.positionY, map.showMapIndex(this.positionY - 1, this.positionX), 0) ||
+            roadAccessableCheck(map.showMapIndex(this.positionY - 1, this.positionX), 0) ||
             // checks down
-            roadAccessableCheck(this.positionY, map.showMapIndex(this.positionY + 1, this.positionX), 6) ||
+            roadAccessableCheck(map.showMapIndex(this.positionY + 1, this.positionX), 6) ||
             // checks left
-            roadAccessableCheck(this.positionX, map.showMapIndex(this.positionY, this.positionX - 1), 0) ||
+            roadAccessableCheck(map.showMapIndex(this.positionY, this.positionX - 1), 0) ||
             //checks right
-            roadAccessableCheck(this.positionX, map.showMapIndex(this.positionY, this.positionX + 1), 6)
+            roadAccessableCheck(map.showMapIndex(this.positionY, this.positionX + 1), 6)
         )
     }
 }
 
-function roadAccessableCheck(objectPosition, object, listIndex) {
-    if (!(objectPosition === listIndex)) {
-        if (object.constructor.name) {
-            if (object.isRoadAccessable) {
-                return true
-            }
+function roadAccessableCheck(object) {
+    if (object.constructor.name) {
+        if (object.isRoadAccessable) {
+            return true
         }
     }
     return false
@@ -74,13 +74,20 @@ let road1 = new Road(0, 3, true)
 let road2 = new Road(0, 4, false)
 let road3 = new Road(0, 2, false)
 let road4 = new Road(1, 3, false)
+
 map.addindex(road1)
 map.addindex(road2)
 map.addindex(road3)
 map.addindex(road4)
 
-console.log(map.showMapIndex(-1, 2))
-
-road3.roadCheck(map)
-
+console.log(map.showMapIndex(0, 4))
 console.log(map.showMapIndex(0, 2))
+console.log(map.showMapIndex(1, 3))
+
+road2.roadCheck(map)
+road3.roadCheck(map)
+road4.roadCheck(map)
+
+console.log(map.showMapIndex(0, 4))
+console.log(map.showMapIndex(0, 2))
+console.log(map.showMapIndex(1, 3))
