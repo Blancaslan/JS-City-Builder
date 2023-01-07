ea50c8a9484698d136d0e55f09d769ef35c844dc
Blancaslan <68610191+Blancaslan@users.noreply.github.com>
2023-01-07 21:51:34 +0000

    isRoadAccessable works but direction checks dont

--- src/road.js
+++ src/road.js
@@ -1,32 +1,66 @@
+// Class for initializing the map and modifying
+class Map {
+    constructor(map) {
+        this.map = map
+    }
+
+    showMap() {
+        console.log(this.map)
+    }
+
+    showMapIndex(y, x) {
+        if (this.map[y][x] === undefined) {
+            return false
+        }
+        return this.map[y][x]
+    }
+
+    deleteIndex(positionX, positionY) {
+        this.map[positionY, positionX] = 0
+    }
+
+    addindex(object) {
+        this.map[object.positionY][object.positionX] = object
+    }
+
+}
+
+// class for making roads and modifying them according to their surroundings
 class Road {
     constructor(positionY, positionX, isRoadAccessable) {
-        this.positionX = positionX
         this.positionY = positionY
+        this.positionX = positionX
         this.isRoadAccessable = isRoadAccessable
     }
 
-    roadCheck(plot) {
+    // checks if a road that is nearby is accessable and if yes then makes this road accessable
+    roadCheck(map) {
         this.isRoadAccessable = (
-            roadAccessableCheck(plot[this.positionY][this.positionX - 1]) || 
-            roadAccessableCheck(plot[this.positionY][this.positionX + 1]) ||
-            roadAccessableCheck(plot[this.positionY - 1][this.positionX]) ||
-            roadAccessableCheck(plot[this.positionY + 1][this.positionX]))
+            // checks up
+            roadAccessableCheck(this.positionY, map.showMapIndex(this.positionY - 1, this.positionX), 0) ||
+            // checks down
+            roadAccessableCheck(this.positionY, map.showMapIndex(this.positionY + 1, this.positionX), 6) ||
+            // checks left
+            roadAccessableCheck(this.positionX, map.showMapIndex(this.positionY, this.positionX - 1), 0) ||
+            //checks right
+            roadAccessableCheck(this.positionX, map.showMapIndex(this.positionY, this.positionX + 1), 6)
+        )
     }
 }
 
-function roadAccessableCheck(object) {
-    if (object.constructor.name) {
-        if (object.isRoadAccessable) {
-            return true
+function roadAccessableCheck(objectPosition, object, listIndex) {
+    if (!(objectPosition === listIndex)) {
+        if (object.constructor.name) {
+            if (object.isRoadAccessable) {
+                return true
+            }
         }
     }
     return false
 }
 
-let road1 = new Road(4, 3, false)
-let road2 = new Road(3, 3, true)
 
-let plot = [
+let map = new Map([
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -34,10 +68,19 @@ let plot = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0]
-]
+])
 
-plot[road1.positionY][road1.positionX]
-plot[road2.positionY][road2.positionX]
-console.log(road1)
-road1.roadCheck(plot)
-console.log(road1)
+let road1 = new Road(0, 3, true)
+let road2 = new Road(0, 4, false)
+let road3 = new Road(0, 2, false)
+let road4 = new Road(1, 3, false)
+map.addindex(road1)
+map.addindex(road2)
+map.addindex(road3)
+map.addindex(road4)
+
+console.log(map.showMapIndex(-1, 2))
+
+road3.roadCheck(map)
+
+console.log(map.showMapIndex(0, 2))
