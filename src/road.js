1c251fa2ac1cb78487240c7ae836a9a995f4f0c2
Blancaslan <68610191+Blancaslan@users.noreply.github.com>
2023-03-05 23:35:27 +0000

    getting rid of old roadCheck in Road class

--- src/road.js
+++ src/road.js
@@ -6,28 +6,4 @@ module.exports = {Road: class Road extends Plot.Plot {
         super(positionY, positionX, isRoadAccessable)
     }
 
-    // checks if a road nearby is accessable then makes this road accessable
-    roadCheck(map) {
-        this.isRoadAccessable = (
-            // checks up
-            roadAccessableCheck(map.showMapIndex(this.positionY - 1, this.positionX)) ||
-            // checks down
-            roadAccessableCheck(map.showMapIndex(this.positionY + 1, this.positionX)) ||
-            // checks left
-            roadAccessableCheck(map.showMapIndex(this.positionY, this.positionX - 1)) ||
-            //checks right
-            roadAccessableCheck(map.showMapIndex(this.positionY, this.positionX + 1))
-        )
-    }
 }}
-
-// make high-way check for a road on each side and when a road is found change that road to roadAccessable, then make that road do roadcheck
-
-function roadAccessableCheck(object) {
-    if (object.constructor.name) {
-        if (object.isRoadAccessable) {
-            return true
-        }
-    }
-    return false
-}
